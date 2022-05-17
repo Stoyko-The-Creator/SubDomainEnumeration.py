56e521c13da211caff78846e969788832d5e2411
Stoyko-The-Creator <104470421+Stoyko-The-Creator@users.noreply.github.com>
2022-05-17 13:13:01 -0700

    Create subdomain_enum.py

---
+++ subdomain_enum.py
@@ -0,0 +1,16 @@
+import sys 
+
+sub_list = open("subdomains.txt").read() # enter wordlist here
+subdoms = sub_list.splitlines()
+
+for sub in subdoms:
+    sub_domains = f"http://{sub}.{sys.argv[1]}" 
+
+    try:
+        requests.get(sub_domains)
+    
+    except requests.ConnectionError: 
+        pass
+    
+    else:
+        print("Valid domain: ",sub_domains)   
